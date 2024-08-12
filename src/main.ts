62eff002a37f32fa9798e996f82650f9a5e5bdd9
DaniilSerga <daniil.serga@gmail.com>
2024-08-12 13:58:42 +0300

    init: initialize project

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+const start = async () => {
+    const PORT = process.env.PORT || 5001;
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Advanced backend course')
+        .setDescription('REST API Documentation')
+        .setVersion('1.0.0')
+        .addTag('Supre')
+        .build();
+    
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api/docs', app, document);
+
+    await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
+
+start();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Get()
+    getAllUsers() {
+        return this.usersService.getAllUsers();
+    }
+
+    @Post()
+    createUser(@Body() userDto: CreateUserDto) {
+        return this.usersService.createUser(userDto);
+    }
+}
